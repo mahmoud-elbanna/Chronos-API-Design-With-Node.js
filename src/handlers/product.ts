744308781918341304eda936e30541ff046ab5af
Mah-moud Elbanna <125469051+mahmoud-elbanna@users.noreply.github.com>
2024-03-27 19:03:13 +0200

    Implement deleteProduct endpoint to remove a product associated with a user

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -57,3 +57,17 @@ export const updateProduct = async (req, res) => {
 
   res.json({data: updated})
 }
+
+// Delete one
+export const deleteProduct = async (req, res) => {
+  const deleted = await prisma.product.delete({
+    where: {
+      id_belongsToId: {
+        id: req.params.id,
+        belongsToId: req.user.id
+      }
+    }
+  })
+
+  res.json({data: deleted})
+}
