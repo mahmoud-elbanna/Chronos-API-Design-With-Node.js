f4ca82f19aac412640eefc853c1fe0cb334e2864
mahmoud-elbanna <melbanna964@gmail.com>
2024-03-26 17:54:43 -0700

    Configure Express application with middleware, routes, and error handling

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import express from 'express'
+import router from './router'
+import morgan from 'morgan'
+import cors from 'cors'
+import { protect } from './modules/auth'
+import { createNewUser, signin } from './handlers/user'
+
+const app = express()
