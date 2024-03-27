aae7ad93bab6d8d84904a09d62f1f82b41d62f93
mohamed <mohamedkharashy27@yahoo.com>
2024-03-27 13:04:10 -0700

    Implement protect middleware to validate JWT tokens and protect routes

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -19,3 +19,32 @@ export const createJWT = (user) => {                 // function to generate a J
   )
   return token                                 // returns the generated JWT token
 }
+
+export const protect = (req, res, next) => {    // middleware function named (protect) to protect routes by requiring a valid JWT (JSON Web Token) in the request headers for authentication.
+  const bearer = req.headers.authorization      // next is a callback to pass control to the next middleware
+                                                // const bearer retrieves the value of the Authorization header from the incoming request, which typically contains the JWT
+  if (!bearer) {                                // checks if the Authorization header is missing (!bearer). If it is missing, it sends a 401 Unauthorized status                                                                              // with a JSON response indicating that the request is not authorized.
+    res.status(401)
+    res.json({message: 'not authorized'})
+    return
+  }
+
+  const [, token] = bearer.split(' ')        // const [, token] is a function splits the Authorization header to extract the token part.
+                                              // if (!token) to checks if a token exists. If it doesn't, it sends a 401 Unauthorized status 
+  if (!token) {                                    // with a JSON response indicating that the token is not valid.
+    res.status(401)
+    res.json({message: 'not valid token'})
+    return
+  }
+                                                          // try block, the user function attempts to verify the JWT using jwt.verify() from the jsonwebtoken library
+  try {                                                      // passing in the token info user and the JWT secret (process.env.JWT_SECRET). 
+    const user = jwt.verify(token, process.env.JWT_SECRET)   // If the token is successfully verified, the user object decoded from the token is attached 
+    req.user = user                                         // to the request (req.user) and the middleware calls next() to pass control to the next middleware
+    next()
+  } catch (e) {                                          // If an error occurs during verification (invalid token or expired token), it catches the error
+    console.error(e)                             // and logs it to the console, sends a 401 Unauthorized status, and returns a JSON response indicating that the token is not valid.
+    res.status(401)
+    res.json({message: 'not valid token'})
+    return
+  }
+}
