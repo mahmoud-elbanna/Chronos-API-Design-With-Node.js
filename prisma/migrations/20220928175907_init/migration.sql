9a9be901a3f64f4188132776e55259b08e40aaaa
Khadija Mohamed <125473499+Khadijamohamed99@users.noreply.github.com>
2024-03-27 02:36:10 +0200

    "UpdatePoint" table's "updateId" references "Update" table's "id"

--- prisma/migrations/20220928175907_init/migration.sql
+++ prisma/migrations/20220928175907_init/migration.sql
@@ -57,3 +57,5 @@ ALTER TABLE "Product" ADD CONSTRAINT "Product_belongsToId_fkey" FOREIGN KEY ("be
 -- AddForeignKey
 ALTER TABLE "Update" ADD CONSTRAINT "Update_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
+-- AddForeignKey
+ALTER TABLE "UpdatePoint" ADD CONSTRAINT "UpdatePoint_updateId_fkey" FOREIGN KEY ("updateId") REFERENCES "Update"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
