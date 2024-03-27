53a87ae663d3ee727e9b19cca14a6e3ca55f4c76
Mah-moud Elbanna <125469051+mahmoud-elbanna@users.noreply.github.com>
2024-03-27 18:45:38 +0200

    Define routes for user registration and authentication

--- src/server.ts
+++ src/server.ts
@@ -11,3 +11,13 @@ app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
+app.get('/', (req, res, next) => {
+  setTimeout(() => {
+    next(new Error('hello'))
+  },1)
+})
+
+app.use('/api', protect, router)
+
+app.post('/user', createNewUser)
+app.post('/signin', signin)
