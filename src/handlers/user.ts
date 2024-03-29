911b55516c1d58882dbdb6c5b007a865e4b8bd39
Mah-moud Elbanna <125469051+mahmoud-elbanna@users.noreply.github.com>
2024-03-29 20:37:55 +0200

    Implement createNewUser endpoint to allow users to register with a username and password
    Hash the user's password before storing it in the database
    Generate and return a JWT token upon successful user registration

---
+++ src/handlers/user.ts
@@ -0,0 +1,14 @@
+import prisma from '../db'
+import { comparePasswords, createJWT, hashPassword } from '../modules/auth'
+
+export const createNewUser = async (req, res) => {
+  const user = await prisma.user.create({
+    data: {
+      username: req.body.username,
+      password: await hashPassword(req.body.password)
+    }
+  })
+
+  const token = createJWT(user)
+  res.json({ token })
+}
